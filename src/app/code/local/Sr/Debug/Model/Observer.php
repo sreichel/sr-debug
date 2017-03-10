6ac438d5605f63979b1e74451efadbbf4502d9d7
Sven Reichel <sven.reichel@hotmail.com>
2017-03-10 02:04:27 +0100

    Added log for ... - adminhtml blocks - eav collection

--- src/app/code/local/Sr/Debug/Model/Observer.php
+++ src/app/code/local/Sr/Debug/Model/Observer.php
@@ -12,63 +12,16 @@
  */
 class Sr_Debug_Model_Observer extends Mage_Core_Model_Observer
 {
-    /**
-     * Classes WITH existing $_eventPrefix
-     *
-     * @var array $existing
-     */
-    private static $existing = array(
-        'Mage_Admin_Model_Roles',
-        'Mage_Admin_Model_User',
-        'Mage_Api_Model_User',
-        'Mage_Catalog_Model_Product_Compare_Item',
-        'Mage_CatalogSearch_Model_Query',
-        'Mage_Core_Model_Config_Data',
-        'Mage_Core_Model_Store',
-        'Mage_Core_Model_Store_Group',
-        'Mage_Core_Model_Website',
-        'Mage_Cms_Model_Page',
-        'Mage_Catalog_Model_Category',
-        'Mage_Catalog_Model_Product',
-        'Mage_Catalog_Model_Resource_Eav_Attribute',
-        'Mage_CatalogRule_Model_Rule',
-        'Mage_Customer_Model_Address',
-        'Mage_Customer_Model_Customer',
-        'Mage_Customer_Model_Group',
-        'Mage_Eav_Model_Entity_Attribute',
-        'Mage_Eav_Model_Entity_Attribute_Set',
-        'Mage_Newsletter_Model_Subscriber',
-        'Mage_Review_Model_Review',
-        'Mage_Sales_Model_Order_Creditmemo',
-        'Mage_Sales_Model_Order_Creditmemo_Item',
-        'Mage_Sales_Model_Order',
-        'Mage_Sales_Model_Order_Address',
-        'Mage_Sales_Model_Order_Invoice',
-        'Mage_Sales_Model_Order_Invoice_Item',
-        'Mage_Sales_Model_Order_Item',
-        'Mage_Sales_Model_Order_Payment',
-        'Mage_Sales_Model_Order_Shipment',
-        'Mage_Sales_Model_Order_Shipment_Item',
-        'Mage_Sales_Model_Order_Shipment_Track',
-        'Mage_Sales_Model_Order_Status_History',
-        'Mage_Sales_Model_Quote',
-        'Mage_Sales_Model_Quote_Address',
-        'Mage_Sales_Model_Quote_Item',
-        'Mage_Sales_Model_Quote_Payment',
-        'Mage_SalesRule_Model_Rule',
-        'Mage_Tag_Model_Tag',
-        'Mage_Widget_Model_Widget_Instance',
-        'Mage_Wishlist_Model_Item',
-        'Mage_Wishlist_Model_Item_Option',
-        'Mage_Wishlist_Model_Wishlist',
-    );
+    const LOG_MODEL = 'model';
+    const LOG_COLLECTION = 'collection';
+    const LOG_ADMINBLOCK = 'adminblock';
 
     /**
-     * Classes WITHOUT existing $_eventPrefix
+     * Tracked classes for PR
      *
      * @var array $new
      */
-    private static $new = array(
+    private static $model = array(
         'Mage_Admin_Model_Block',
         'Mage_Admin_Model_Variable',
         'Mage_Adminhtml_Model_Email_Template',
@@ -79,6 +32,7 @@ class Sr_Debug_Model_Observer extends Mage_Core_Model_Observer
         'Mage_Api2_Model_Acl_Global_Rule',
         'Mage_Bundle_Model_Option',
         'Mage_Bundle_Model_Selection',
+        'Mage_Catalog_Model_Product_Flat_Flag',
         'Mage_Catalog_Model_Product_Option',
         'Mage_Catalog_Model_Product_Option_Value',
         'Mage_Catalog_Model_Product_Type_Configurable_Attribute',
@@ -86,18 +40,25 @@ class Sr_Debug_Model_Observer extends Mage_Core_Model_Observer
         'Mage_Checkout_Model_Agreement',
         'Mage_Cms_Model_Block',
         'Mage_Core_Model_Design',
+        'Mage_Core_Model_Flag',
+        'Mage_Core_Model_File_Storage_Flag',
         'Mage_Core_Model_Email_Queue',
         'Mage_Core_Model_Url_Rewrite',
         'Mage_Core_Model_Variable',
         'Mage_Customer_Model_Flowpassword',
         'Mage_Dataflow_Model_Profile',
         'Mage_Dataflow_Model_Profile_History',
+        'Mage_Directory_Model_Country',
+        'Mage_Directory_Model_Region',
         'Mage_Downloadable_Model_Link',
+        'Mage_Downloadable_Model_Link_Purchased',
+        'Mage_Downloadable_Model_Link_Purchased_Item',
         'Mage_Eav_Model_Entity_Attribute_Group',
         'Mage_Eav_Model_Entity_Store',
         'Mage_GiftMessage_Model_Message',
         'Mage_Index_Model_Event',
         'Mage_Index_Model_Process',
+        'Mage_Log_Model_Customer',
         'Mage_Log_Model_Visitor',
         'Mage_Oauth_Model_Consumer',
         'Mage_Poll_Model_Poll',
@@ -107,6 +68,7 @@ class Sr_Debug_Model_Observer extends Mage_Core_Model_Observer
         'Mage_Reports_Model_Flag',
         'Mage_Reports_Model_Product_Index_Compared',
         'Mage_Reports_Model_Product_Index_Viewed',
+        'Mage_Review_Model_Review_Summary',
         'Mage_Sales_Model_Order_Creditmemo_Comment',
         'Mage_Sales_Model_Order_Invoice_Comment',
         'Mage_Sales_Model_Order_Shipment_Comment',
@@ -122,10 +84,43 @@ class Sr_Debug_Model_Observer extends Mage_Core_Model_Observer
         'Mage_Tax_Model_Calculation_Rate',
         'Mage_Tax_Model_Calculation_Rule',
         'Mage_Wishlist_Model_Item_Option',
+        'Mage_Wishlist_Model_Wishlist',
+    );
+
+    private static $collection = array(
+        'Mage_Catalog_Model_Resource_Collection_Abstract',
+        'Mage_Customer_Model_Resource_Address_Collection',
+        'Mage_Customer_Model_Resource_Customer_Collection',
+        'Mage_Customer_Model_Resource_Wishlist_Collection',
+        'Mage_Eav_Model_Entity_Collection',
+        'Mage_Sales_Model_Entity_Order_Collection',
+        'Mage_Sales_Model_Entity_Order_Address_Collection',
+        'Mage_Sales_Model_Entity_Order_Creditmemo_Collection',
+        'Mage_Sales_Model_Entity_Order_Creditmemo_Comment_Collection',
+        'Mage_Sales_Model_Entity_Order_Creditmemo_Item_Collection',
+        'Mage_Sales_Model_Entity_Order_Invoice_Collection',
+        'Mage_Sales_Model_Entity_Order_Invoice_Comment_Collection',
+        'Mage_Sales_Model_Entity_Order_Invoice_Item_Collection',
+        'Mage_Sales_Model_Entity_Order_Item_Collection',
+        'Mage_Sales_Model_Entity_Order_Payment_Collection',
+        'Mage_Sales_Model_Entity_Order_Shipment_Collection',
+        'Mage_Sales_Model_Entity_Order_Shipment_Comment_Collection',
+        'Mage_Sales_Model_Entity_Order_Shipment_Item_Collection',
+        'Mage_Sales_Model_Entity_Order_Shipment_Track_Collection',
+        'Mage_Sales_Model_Entity_Order_Status_History_Collection',
+        'Mage_Sales_Model_Entity_Quote_Collection',
+        'Mage_Sales_Model_Entity_Quote_Address_Collection',
+        'Mage_Sales_Model_Entity_Quote_Address_Item_Collection',
+        'Mage_Sales_Model_Entity_Quote_Address_Rate_Collection',
+        'Mage_Sales_Model_Entity_Quote_Item_Collection',
+        'Mage_Sales_Model_Entity_Quote_Payment_Collection',
+    );
+
+    private static $adminblock = array(
     );
 
     /**
-     * Log $_event-prefix
+     * Log model classes WITHOUT $_event-prefix
      *
      * @param Varien_Event_Observer $observer
      * @return void
@@ -133,17 +128,47 @@ class Sr_Debug_Model_Observer extends Mage_Core_Model_Observer
     public function logEventPrefix(Varien_Event_Observer $observer)
     {
         $object = $observer->getEvent()->getObject();
-        $class = get_class($object);
+        $this->_log(get_class($object), self::LOG_MODEL);
+    }
 
-        $exclude = array_merge(self::$existing, self::$new);
-        if (!in_array($class, $exclude)) {
-            $reflection = new ReflectionClass($object);
-            $prefix = $reflection->getProperty('_eventPrefix');
-            $prefix->setAccessible(true);
-            $value = $prefix->getValue($object);
+    /**
+     * Log eav collection classes WITHOUT $_event-prefix
+     *
+     * @param Varien_Event_Observer $observer
+     * @return void
+     */
+    public function logEventPrefixCollection(Varien_Event_Observer $observer)
+    {
+        $object = $observer->getEvent()->getCollection();
+        $this->_log(get_class($object), self::LOG_COLLECTION);
+    }
 
-            $logFile = ($value == 'core_abstract') ? 'event-prefix_NEW.log' : 'event-prefix_CORE.log';
-            Mage::log($class, null, $logFile);
+    /**
+     * Log adminhtml block classes WITHOUT $_event-prefix
+     *
+     * @param Varien_Event_Observer $observer
+     * @return void
+     */
+    public function logEventPrefixBlock(Varien_Event_Observer $observer)
+    {
+        $object = $observer->getEvent()->getBlock();
+        $this->_log(get_class($object), self::LOG_ADMINBLOCK);
+    }
+
+    /**
+     * Write logfile
+     *
+     * @param string $class
+     * @param string $type
+     */
+    private function _log($class, $type)
+    {
+        if (Mage::getStoreConfigFlag('dev/event_prefix_log_' . $type . '/active')) {
+            if (!in_array($class, self::$$type)) {
+                if (substr($class, 0, 5) === 'Mage_') {
+                    Mage::log($class, null, "event-prefix-{$type}.log", true);
+                }
+            }
         }
     }
 }
